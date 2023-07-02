14c6b668849880a23e7dce12bc534d4228d32fd8
igor-rizak <igor.rizak@ukr.net>
2023-07-02 23:43:58 +0300

    1

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { nanoid } from "nanoid";
+import { Form, Label, Button, Input } from './ContactForm.styled';
+
+class ContactForm extends React.Component {
+    state = {
+        name: '',
+        number: '',
+    };
+    
+}
